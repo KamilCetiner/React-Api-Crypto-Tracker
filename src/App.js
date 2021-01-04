4fdc128029e229e3065d9a48ea5a6b994742213a
KamilCetiner <cetiner.kamil84@gmail.com>
2021-01-04 20:24:57 +0100

    mapping the list and filtered

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ function App() {
   
 
     const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
+
     
   
     useEffect(() => {
@@ -13,7 +15,17 @@ function App() {
         setCoins(res.data)
       }).catch(error => console.log(error))
    
-    }, [])
+    }, []);
+
+    const handleChange = e => {
+        setSearch(e.target.value)
+       
+      }
+    
+      const filteredCoins = coins.filter(coin => 
+        coin.name.toLowerCase().includes(search.toLowerCase())
+    
+      )
   
     
     return (
@@ -26,6 +38,26 @@ function App() {
             className="coin-input" onChange={handleChange} />
           </form>
         </div>
+
+        {filteredCoins.map(coin => {
+        return(
+
+          <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          marketcap={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+
+          />
+
+
+        )
+
+      })}
   
       </div>
     );
